85a5dadaa17b7b8fd33d2010274b861448ccfb3e
Hlozi <xhloze02@vutbr.cz>
2022-04-28 10:05:21 +0200

    Added methods for Car and Ride Facedes

--- RideShare.BL/Facades/CarFacade.cs
+++ RideShare.BL/Facades/CarFacade.cs
@@ -15,5 +15,36 @@ namespace RideShare.BL.Facades
         public CarFacade(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper) : base(unitOfWorkFactory, mapper)
         {
         }
+
+        public async Task CreateNewCar(UserDetailModel Owner, DateTime RegDate, string Brand, string Type, int Seats, string ImagePath = null)
+        {
+            var car = new CarDetailModel(
+                Id: Guid.NewGuid(),
+                RegDate: DateTime.Now,
+                Brand: Brand,
+                Type: Type,
+                Seats: Seats,
+                ImagePath: ImagePath,
+                UserId: Owner.Id);
+            await SaveAsync(car);
+        }
+
+        public async Task EditCar(CarDetailModel Car, int Seats, string Brand = null, string Type = null, string ImagePath = null)
+        {
+            Car.Seats = Seats;
+            if (Brand != null)
+            {
+                Car.Brand = Brand;
+            }
+            if (Type != null)
+            {
+                Car.Type = Type;
+            }
+            if (ImagePath != null)
+            {
+                Car.ImagePath = ImagePath;
+            }
+            await SaveAsync(Car);
+        }
     }
 }
--- RideShare.BL/Facades/RideFacade.cs
+++ RideShare.BL/Facades/RideFacade.cs
@@ -7,14 +7,16 @@ using AutoMapper;
 using RideShare.BL.Models;
 using RideShare.DAL.Entities;
 using RideShare.DAL.UnitOfWork;
+using RideShare.BL.Facades;
 
 namespace RideShare.BL.Facades
 {
     public class RideFacade : CRUDFacade<RideEntity, RideListModel, RideDetailModel>
     {
+        private readonly RideUserFacade _rideUserFacadeSUT;
         public RideFacade(IUnitOfWorkFactory unitOfWorkFactory, IMapper mapper) : base(unitOfWorkFactory, mapper)
         {
-
+            _rideUserFacadeSUT = new RideUserFacade(unitOfWorkFactory, mapper);
         }
 
         public async Task CreateRide(UserDetailModel Driver, CarDetailModel Car, string StartLocation, string Destination,
@@ -34,6 +36,65 @@ namespace RideShare.BL.Facades
             await SaveAsync(newRide);
         }
 
+        public async Task DeleteRide(RideDetailModel Ride)
+        {
+            foreach(RideUserModel RideUser in Ride.RideUsers)
+            {
+                await _rideUserFacadeSUT.DeleteAsync(RideUser);
+            }
+            await DeleteAsync(Ride);
+        }
 
+        public async Task AddPassengerToRide(RideDetailModel Ride, UserDetailModel Passenger)
+        {
+            var Passengers = Ride.RideUsers;
+            if (Ride.Occupancy == Passengers.Count)
+            {
+                throw new Exception("Ride is full");
+            }
+            var OtherRides = Passenger.RideUsers;
+            foreach (var OtherRideUser in OtherRides)
+            {
+                var OtherRide = await GetAsync(OtherRideUser.RideId);
+                if (OtherRide.StartTime >= Ride.StartTime)
+                {
+                }
+            }
+
+            var RideUser = new RideUserModel(
+                Id: Guid.NewGuid(),
+                UserId: Passenger.Id,
+                RideId: Ride.Id);
+            await _rideUserFacadeSUT.SaveAsync(RideUser);  // maybe a problem
+            Ride.RideUsers.Add(RideUser);
+            Ride.Occupancy++;
+            await SaveAsync(Ride);
+        }
+
+        public async Task DeletePassengerFromRide(RideUserModel RideUser)
+        {
+            var Ride = await GetAsync(RideUser.RideId);
+            Ride.RideUsers.Remove(RideUser);
+            await _rideUserFacadeSUT.DeleteAsync(RideUser);
+            Ride.Occupancy--;
+            await SaveAsync(Ride);
+        }
+
+        public async Task<List<RideListModel>> FilterRides(DateTime StartTime, DateTime EstEndTime, string StartLocation = null, string Destination = null)
+        {
+            var Rides = GetAsync().Result;
+            if (StartLocation != null)
+            {
+                Rides = Rides.Where(x => x.StartLocation == StartLocation);
+            }
+            if (Destination != null)
+            {
+                Rides = Rides.Where(x => x.Destination == Destination);
+            }
+            Rides = Rides.Where(x => x.StartTime >= StartTime);
+            Rides = Rides.Where(x => x.EstEndTime <= EstEndTime);
+            return Rides.ToList();
+            
+        }
     }
 }
